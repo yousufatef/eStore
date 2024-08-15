54fa5b76c1782893ebe4c5f4a28e20521fc2d199
youssef <youssefatef@gmail.com>
2024-08-15 17:58:09 +0300

    product and home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import "./App";
 import Header from "./components/header/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <Container>
         <main className="py-3">
-          <h1>Welocome</h1>
+          <HomePage />
         </main>
       </Container>
       <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
