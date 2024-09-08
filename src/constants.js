ee9f3265a36063156dd6d5b172ff07d2c471381a
yousufatef <joeatef769@gmail.com>
2024-09-09 01:02:36 +0300

    update

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "https://estore-backend-production.up.railway.app/";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
